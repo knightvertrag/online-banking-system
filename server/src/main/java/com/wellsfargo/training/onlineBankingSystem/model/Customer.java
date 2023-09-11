5908007c29bab54f908a8bc05c12c9adb32a3fa4
Satyam Pareek <pareeksatyam01@gmail.com>
2023-09-11 14:18:30 +0530

    created database and Customer pojo class

--- server/src/main/java/com/wellsfargo/training/onlineBankingSystem/model/Customer.java
+++ server/src/main/java/com/wellsfargo/training/onlineBankingSystem/model/Customer.java
@@ -1,21 +1,79 @@
 package com.wellsfargo.training.onlineBankingSystem.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.SequenceGenerator;
+
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Customer {
 
-    @Id
-    private String id;
+	@SequenceGenerator(name="customer_seq",initialValue = 1000,allocationSize = 1)
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY,generator="customer_seq")
+    private Long id;
+	
+	@Column(nullable=false)
     private String firstName;
+	
+	@Column(nullable=false)
     private String lastName;
+	
+	@Column(nullable=false)
     private String phone;
+	
+	
+	
+	public Customer() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public Customer(Long id, String firstName, String lastName, String phone) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phone = phone;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	
 
 }
