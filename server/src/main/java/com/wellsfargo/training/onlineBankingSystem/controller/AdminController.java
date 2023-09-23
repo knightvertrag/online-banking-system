51a09607b91b20b161666b767af1f178de3bfe64
Satyam Pareek <pareeksatyam01@gmail.com>
2023-09-23 17:59:19 +0530

    view prifle + edit profile + backend integration

--- server/src/main/java/com/wellsfargo/training/onlineBankingSystem/controller/AdminController.java
+++ server/src/main/java/com/wellsfargo/training/onlineBankingSystem/controller/AdminController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import com.wellsfargo.training.onlineBankingSystem.repository.AccountRepository;
 import com.wellsfargo.training.onlineBankingSystem.service.AccountService;
 import com.wellsfargo.training.onlineBankingSystem.service.AdminService;
 
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping(value="/admin")
 public class AdminController {
--- server/src/main/java/com/wellsfargo/training/onlineBankingSystem/controller/TransactionController.java
+++ server/src/main/java/com/wellsfargo/training/onlineBankingSystem/controller/TransactionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import com.wellsfargo.training.onlineBankingSystem.model.Transaction;
 import com.wellsfargo.training.onlineBankingSystem.model.TransactionRequest;
 import com.wellsfargo.training.onlineBankingSystem.service.TransactionService;
 
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping("/transactions")
 public class TransactionController {
