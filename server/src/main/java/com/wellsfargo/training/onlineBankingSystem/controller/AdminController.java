d2d22c7878397eded280b99288f22104cabdd98e
Satyam Pareek <pareeksatyam01@gmail.com>
2023-09-23 12:49:13 +0530

    login done for admin

--- server/src/main/java/com/wellsfargo/training/onlineBankingSystem/controller/AdminController.java
+++ server/src/main/java/com/wellsfargo/training/onlineBankingSystem/controller/AdminController.java
@@ -5,12 +5,18 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wellsfargo.training.onlineBankingSystem.exception.NoSuchCustomerExistsException;
 import com.wellsfargo.training.onlineBankingSystem.model.Account;
+import com.wellsfargo.training.onlineBankingSystem.model.Admin;
 import com.wellsfargo.training.onlineBankingSystem.model.Customer;
 import com.wellsfargo.training.onlineBankingSystem.repository.AccountRepository;
 import com.wellsfargo.training.onlineBankingSystem.service.AccountService;
@@ -28,6 +34,25 @@ public class AdminController {
 	
 	@Autowired
 	public AccountRepository accRepository;
+	
+	@PostMapping("/loginAdmin")
+	public Boolean loginAdmin(@Validated @RequestBody Admin admin ) throws NoSuchCustomerExistsException {
+		Boolean isLoggedIn=false;
+		Long adminId=admin.getAdminId();
+		String password=admin.getPassword();
+		
+		Admin ad= adminService.loginAdmin(adminId).orElseThrow(()->
+		new NoSuchCustomerExistsException("Admin Not Found for this ID::"));
+		
+		if(adminId.equals(ad.getAdminId()) && password.equals(ad.getPassword()))
+				{
+					isLoggedIn=true;
+				}
+		return isLoggedIn;
+				
+		
+		
+	}
 	
 	@GetMapping("/allCustomers")
 	public ResponseEntity<List<Customer>> fetchAllCustomers(){
