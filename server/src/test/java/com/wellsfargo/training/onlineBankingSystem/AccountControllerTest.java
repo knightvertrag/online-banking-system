30941f945deac97db9b1f48b1c55e7b9d5c46bb6
Satyam Pareek <pareeksatyam01@gmail.com>
2023-09-27 11:04:56 +0530

    added controller tests

--- server/src/test/java/com/wellsfargo/training/onlineBankingSystem/AccountControllerTest.java
+++ server/src/test/java/com/wellsfargo/training/onlineBankingSystem/AccountControllerTest.java
@@ -1,5 +1,152 @@
 package com.wellsfargo.training.onlineBankingSystem;
 
-public class AccountControllerTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wellsfargo.training.onlineBankingSystem.controller.AccountController;
+import com.wellsfargo.training.onlineBankingSystem.exception.NoSuchCustomerExistsException;
+import com.wellsfargo.training.onlineBankingSystem.model.Account;
+import com.wellsfargo.training.onlineBankingSystem.model.Customer;
+import com.wellsfargo.training.onlineBankingSystem.repository.CustomerRepository;
+import com.wellsfargo.training.onlineBankingSystem.service.AccountService;
+
+@SpringBootTest
+public class AccountControllerTest{
+	
+	private MockMvc mockMvc;
+	
+	@InjectMocks
+	private AccountController accController;
+	
+	@Mock
+	private AccountService accService;
+	
+	@Mock
+	private CustomerRepository custRepository;
+	
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		mockMvc=MockMvcBuilders.standaloneSetup(accController).build();
+	}
+	
+	@Test
+	public void testCreateAccount() throws Exception {
+		
+		Long custId=1L;
+		Account account = new Account();
+		Customer customer = new Customer();
+		
+		account.setBalance(100);
+		account.setIsActive(1);
+		account.setTransPassword("transpassword");
+		account.setAccNo(100L);
+		
+		customer.setAadhar("123456789012");
+		customer.setCustId(custId);
+		customer.setDob(null);
+		customer.setEmail("satyam@example.com");
+		customer.setFatherName("father");
+		customer.setFirstName("satyam");
+		customer.setLastName("pareek");
+		customer.setPassword("password");
+		customer.setPhone("9876543211");
+		
+		account.setCustomer(customer);
+		
+		when(accService.createAccount(eq(custId), any(Account.class))).thenReturn(account);
+		
+		mockMvc.perform(post("/accounts/createAccount/{custId}",custId)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(account)))
+				.andExpect(status().isOk())
+				.andExpect(content().string("New Account Created Successfully"));
+		
+		
+		
+	}
+	
+	@Test
+	public void testGetAllAccounts() throws Exception {
+	
+		Long custId=1L;
+		Customer customer=new Customer();
+		
+		List<Account> accounts = new ArrayList<>();
+		Account a1= new Account();
+		Account a2= new Account();
+		
+		a1.setBalance(100);
+		a1.setIsActive(1);
+		a1.setTransPassword("transpassword");
+		a1.setAccNo(100L);
+		
+		a2.setBalance(1000);
+		a2.setIsActive(1);
+		a2.setTransPassword("transpassword");
+		a2.setAccNo(101L);
+		
+		customer.setAadhar("123456789012");
+		customer.setCustId(custId);
+		customer.setDob(null);
+		customer.setEmail("satyam@example.com");
+		customer.setFatherName("father");
+		customer.setFirstName("satyam");
+		customer.setLastName("pareek");
+		customer.setPassword("password");
+		customer.setPhone("9876543211");
+		
+		
+		
+		a1.setCustomer(customer);
+		a2.setCustomer(customer);
+		
+		accounts.add(a1);
+		accounts.add(a2);
+		
+		customer.setAccounts(accounts);
+		
+		when(custRepository.findById(eq(custId))).thenReturn(Optional.of(customer));
+		when(accService.getAllAccounts(eq(custId))).thenReturn(accounts);
+		
+		mockMvc.perform(get("/accounts/getAccounts/{custId}",custId))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.size()").value(accounts.size()))
+				.andExpect(jsonPath("$[0].balance").value(accounts.get(0).getBalance()));	
+		
+		
+	}
+
+		
+	
 }
--- server/src/test/java/com/wellsfargo/training/onlineBankingSystem/AdminControllerTest.java
+++ server/src/test/java/com/wellsfargo/training/onlineBankingSystem/AdminControllerTest.java
@@ -1,5 +1,141 @@
 package com.wellsfargo.training.onlineBankingSystem;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wellsfargo.training.onlineBankingSystem.controller.AdminController;
+import com.wellsfargo.training.onlineBankingSystem.model.Account;
+import com.wellsfargo.training.onlineBankingSystem.model.Admin;
+import com.wellsfargo.training.onlineBankingSystem.model.Customer;
+import com.wellsfargo.training.onlineBankingSystem.repository.AccountRepository;
+import com.wellsfargo.training.onlineBankingSystem.repository.CustomerRepository;
+import com.wellsfargo.training.onlineBankingSystem.service.AccountService;
+import com.wellsfargo.training.onlineBankingSystem.service.AdminService;
+
+@SpringBootTest
 public class AdminControllerTest {
 
+private MockMvc mockMvc;
+	
+	@InjectMocks
+	private AdminController adminController;
+	
+	@Mock
+	private AccountService accService;
+	@Mock
+	private AdminService adminService;
+	@Mock
+	private AccountRepository accRepository;
+	
+	@Mock
+	private CustomerRepository custRepository;
+	
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		mockMvc=MockMvcBuilders.standaloneSetup(adminController).build();
+	}
+	
+	@Test
+	public void testLoginAdmin() throws Exception {
+		Long adminId=1L;
+		Admin admin = new Admin();
+		admin.setAdminId(adminId);
+		admin.setPassword("password");
+		
+		when(adminService.loginAdmin(eq(adminId))).thenReturn(Optional.of(admin));
+		
+		mockMvc.perform(post("/admin/loginAdmin")
+		.contentType(MediaType.APPLICATION_JSON)
+		.content(new ObjectMapper().writeValueAsString(admin)))
+		.andExpect(status().isOk())
+		.andExpect(content().string("true"));
+		
+	}
+	
+	@Test
+	public void testFetchAllCustomers() throws Exception {
+		Customer customer1=new Customer();
+		Customer customer2=new Customer();
+		
+		customer1.setAadhar("123456789012");
+		customer1.setCustId(1L);
+		customer1.setDob(null);
+		customer1.setEmail("satyam@example.com");
+		customer1.setFatherName("father");
+		customer1.setFirstName("satyam");
+		customer1.setLastName("pareek");
+		customer1.setPassword("password");
+		customer1.setPhone("9876543211");
+		
+		customer2.setAadhar("123456789012");
+		customer2.setCustId(1L);
+		customer2.setDob(null);
+		customer2.setEmail("satyam@example.com");
+		customer2.setFatherName("father");
+		customer2.setFirstName("satyam");
+		customer2.setLastName("pareek");
+		customer2.setPassword("password");
+		customer2.setPhone("9876543211");
+		
+		List<Customer> customers= new ArrayList<Customer>();
+		customers.add(customer1);
+		customers.add(customer2);
+		
+		when(adminService.getAllCustomers()).thenReturn(customers);
+		
+		mockMvc.perform(get("/admin/allCustomers"))
+		.andExpect(status().isOk())
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$.size()").value(customers.size()));
+		
+		
+		
+	}
+	
+	@Test
+	public void testDeactivateCustomerAccount() throws Exception {
+		Long accNo=100L;
+		Account acc = new Account();
+		
+		acc.setBalance(100);
+		acc.setIsActive(1);
+		acc.setTransPassword("transpassword");
+		acc.setAccNo(accNo);
+		
+		
+		when(accService.getSingleAccount(accNo)).thenReturn(Optional.of(acc) );
+		mockMvc.perform(get("/admin/deactivateAccount/{accNo}",accNo))
+		.andExpect(status().isOk())
+		.andExpect(content().string("Account with Account Number "+accNo+" has been deactivated"));
+		
+		Mockito.verify(accRepository).save(any(Account.class));
+		
+	}
+
+	
 }
+
